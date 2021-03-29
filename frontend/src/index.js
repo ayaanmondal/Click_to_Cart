855491e26530201b081766bb6f6ae36dfd410a2d
ayaanmondal <ayanmondal9929@gmail.com>
2021-03-30 01:42:36 +0530

    Create Redux Store

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,10 +6,14 @@ import reportWebVitals from './reportWebVitals';
 
 import './bootstrap.min.css'
 
+
+import { Provider } from 'react-redux'
+import store from './store'
+
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
