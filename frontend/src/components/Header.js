cbe1f363ab6c37e38fcf269c00c9af00aeb5ae6c
ayaanmondal <ayanmondal9929@gmail.com>
2021-04-23 14:07:12 +0530

    User Logout

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,10 +1,22 @@
 import React from "react";
 
-import { Navbar, Nav, Container, Row } from "react-bootstrap";
+import { Navbar, Nav, Container, Row, NavDropdown } from "react-bootstrap";
 
 import { LinkContainer } from "react-router-bootstrap";
+import { useDispatch, useSelector } from 'react-redux'
+
+import { logout } from '../actions/userActions'
 
 function Header() {
+
+  const userLogin = useSelector(state => state.userLogin)
+  const { userInfo } = userLogin
+
+  const dispatch = useDispatch()
+
+  const logoutHandler = () => {
+    dispatch(logout())
+  }
   return (
     <header>
       <container>
@@ -27,11 +39,22 @@ function Header() {
                   <i className="fas fa-shopping-cart"></i> Cart
                 </Nav.Link>
               </LinkContainer>
-              <LinkContainer to="/login">
+              { userInfo ? (
+                <NavDropdown title={userInfo.name} id='username'>
+                  <LinkContainer to='/profile'>
+                    <NavDropdown.Item>Profile</NavDropdown.Item>
+                  </LinkContainer>
+                  <NavDropdown.Item onClick={logoutHandler}>Logout</NavDropdown.Item>
+                </NavDropdown>
+              ) : (
+                <LinkContainer to="/login">
                 <Nav.Link>
                   <i className="fas fa-user"></i> Login
                 </Nav.Link>
               </LinkContainer>
+              )}
+
+              
             </Nav>
           </Navbar.Collapse>
         </Navbar>
