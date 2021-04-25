55729bfd0c50356df2ff7fc5c3558ddec556e29e
ayaanmondal <ayanmondal9929@gmail.com>
2021-04-25 15:31:25 +0530

    Order pay backend

--- backend/base/views/order_views.py
+++ backend/base/views/order_views.py
@@ -7,6 +7,7 @@ from rest_framework.response import Response
 from base.models import Product, Order, OrderItem, ShippingAddress
 from base.serializers import ProductSerializer, OrderSerializer
 from rest_framework import status
+from datetime import datetime
 
 @api_view(['POST'])
 @permission_classes([IsAuthenticated])
@@ -75,3 +76,14 @@ def getOrderById(request, pk):
     except:
         return Response({'detail':'Oredr does not exist'},
                         status= status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['PUT'])
+@permission_classes([IsAuthenticated])
+def updateOrderToPaid(request, pk):
+    order = Order.objects.get(_id=pk)
+
+    order.isPaid = True
+    order.paidAt = datetime.now()
+    order.save()
+    return Response('Order was paid')
